ef71a1927a5b03748e9834a20571af6206b9d090
enimalojd <enderston8@gmail.com>
2024-05-04 16:25:01 +0300

    fixs models for migrations

--- task_manager/apps/tasks/models.py
+++ task_manager/apps/tasks/models.py
@@ -1,21 +1,31 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from task_manager.apps.labels.models import Label
+from task_manager.apps.statuses.models import Status
+from task_manager.apps.users.models import User
+
 
 class Task(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False, unique=True)
     description = models.TextField(null=True, blank=True)
     status = models.ForeignKey(
-        "statuses.Status", on_delete=models.PROTECT, null=False, blank=False
+        Status, on_delete=models.PROTECT, null=False, blank=False
     )
     executor = models.ForeignKey(
-        "users.User", on_delete=models.PROTECT, null=True, blank=True
+        User,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="executor",
     )
-    labels = models.ManyToManyField(
-        "labels.Label", blank=True, related_name="tasks", verbose_name=_("Тэги")
-    )
+    labels = models.ManyToManyField(Label, blank=True)
     author = models.ForeignKey(
-        "users.User", on_delete=models.PROTECT, null=False, blank=False
+        User,
+        on_delete=models.PROTECT,
+        null=False,
+        blank=False,
+        related_name="author",
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
